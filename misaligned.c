0be32e9eae592bd3e731886e1d47c1ec8620e5cc
Prarthna-Grace <prarthnagrace31@gmail.com>
2024-10-18 13:31:10 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -16,6 +16,36 @@ int printColorMap() {
 int main() {
     int result = printColorMap();
     assert(result == 25);
+     // Expected output
+    const char* expectedOutput =
+        "0 | White | Blue\n"
+        "1 | White | Orange\n"
+        "2 | White | Green\n"
+        "3 | White | Brown\n"
+        "4 | White | Slate\n"
+        "5 | Red | Blue\n"
+        "6 | Red | Orange\n"
+        "7 | Red | Green\n"
+        "8 | Red | Brown\n"
+        "9 | Red | Slate\n"
+        "10 | Black | Blue\n"
+        "11 | Black | Orange\n"
+        "12 | Black | Green\n"
+        "13 | Black | Brown\n"
+        "14 | Black | Slate\n"
+        "15 | Yellow | Blue\n"
+        "16 | Yellow | Orange\n"
+        "17 | Yellow | Green\n"
+        "18 | Yellow | Brown\n"
+        "19 | Yellow | Slate\n"
+        "20 | Violet | Blue\n"
+        "21 | Violet | Orange\n"
+        "22 | Violet | Green\n"
+        "23 | Violet | Brown\n"
+        "24 | Violet | Slate\n";
+
+    // Validate output content
+    assert(strcmp(output, expectedOutput) == 0);
     printf("All is well (maybe!)\n");
     return 0;
 }
