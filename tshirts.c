48ed91c8aafe1f0e6af807059e4dca8ff268b1ed
Prarthna-Grace <prarthnagrace31@gmail.com>
2024-10-18 12:46:52 +0530

    Update tshirts.c

--- tshirts.c
+++ tshirts.c
@@ -14,9 +14,16 @@ char size(int cms) {
 }
 
 int main() {
+    
+    assert(size(20) == 'S'); //Lower limit is not included which could be a error 
     assert(size(37) == 'S');
+    assert(size(38) == 'S'); //Size 38 is not addressed
+    assert(size(38) == 'M'); //Size 38 is not addressed
     assert(size(40) == 'M');
-    assert(size(43) == 'L');
+    assert(size(42) == 'M');
+    assert(size(42) == 'L'); //Size 42 is not addressed
+    assert(size(43) == 'L'); //Size 42 is not addressed
+    assert(size(60) == 'S'); //Higher limit is not included which could be a error 
     printf("All is well (maybe!)\n");
     return 0;
 }
